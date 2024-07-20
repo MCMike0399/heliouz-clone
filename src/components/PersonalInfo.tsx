0414f95dd0ecd6268a65a3f82aeff2942853f14a
LuisFernando <ferchopwos777@gmail.com>
2024-07-20 12:37:50 -0600

    Merge Main

--- src/components/PersonalInfo.tsx
+++ src/components/PersonalInfo.tsx
@@ -1,13 +1,12 @@
-import React from 'react';
+import React from "react";
 
 const PersonalInfo: React.FC = () => {
-  return (
-    <section id="info">
-      <h2>Información General</h2>
-      <p>Aquí va la información general de la persona.</p>
-      
-    </section>
-  );
-}
+   return (
+      <section id="info">
+         <h2>Información General</h2>
+         <p>Aquí va la información general de la persona.</p>
+      </section>
+   );
+};
 
 export default PersonalInfo;
